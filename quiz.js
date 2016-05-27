5ffb30ad5275e2e94c655c509e147007d3d7fff9
Dale Severude <dseverude@msn.com>
2016-05-27 14:53:33 -0700

    major cleanup of code

--- quiz.js
+++ quiz.js
@@ -8,16 +8,16 @@ var numCorrect = 0;
 alert('Prepare to be tested with an advanced geography challenge.  You have been warned!');
 
 //question number 1
-q1 = prompt("Which is larger: '1' the lower 48 states or '2' Antartica?");
+q1 = prompt("Which is larger: '1' the lower 48 contiguous United States or '2' Antartica?");
 if (parseInt(q1) === 2) {
-  document.write('<p>Correct: The lower 48 states are 0.58 times smaller than Antactica</p>');
+  document.write('<p>Correct: Antactica is 1.73 times larger than the lower 48 states</p>');
   numCorrect +=1;
 } else {
-  document.write('<p>Sorry: The lower 48 states are 0.58 times smaller than Antactica</p>');
+  document.write('<p>Sorry: Antactica is 1.73 times larger than the lower 48 states</p>');
 }
 
 //question number 2
-q2 = prompt("Which is larger: '1' the lower 48 states or '2' Austrialia?");
+q2 = prompt("Which is larger: '1' the lower 48 contiguous United States or '2' Austrialia?");
 if (parseInt(q2) === 1) {
   document.write('<p>Correct: The lower 48 states are 1.05 times larger than Austrailia</p>');
   numCorrect +=1;
@@ -26,25 +26,25 @@ if (parseInt(q2) === 1) {
 }
 
 //question number 3
-q3 = prompt("Which is larger: '1' the lower 48 states or '2' Brazil?");
+q3 = prompt("Which is larger: '1' the lower 48 contiguous United States or '2' Brazil?");
 if (parseInt(q3) === 2) {
-  document.write('<p>Correct: The lower 48 states are 0.95 times smaller than Brazil</p>');
+  document.write('<p>Correct: Brazil is 1.05 times larger than the lower 48 states</p>');
   numCorrect +=1;
 } else {
-  document.write('<p>Sorry: The lower 48 states are 0.95 times smaller than Brazil</p>');
+  document.write('<p>Sorry: Brazil is 1.05 times larger than the lower 48 states</p>');
 }
 
 //question number 4
-q4 = prompt("Which is larger: '1' the lower 48 states or '2' China?");
+q4 = prompt("Which is larger: '1' the lower 48 contiguous United States or '2' China?");
 if (parseInt(q4) === 2) {
-  document.write('<p>Correct: The lower 48 states are 0.84 times smaller than China</p>');
+  document.write('<p>Correct: China is 1.19 times larger than the lower 48 states</p>');
   numCorrect +=1;
 } else {
-  document.write('<p>Sorry: The lower 48 states are 0.84 times smaller than China</p>');
+  document.write('<p>Sorry: China is 1.19 times larger than the lower 48 states</p>');
 }
 
 //question number 5
-q5 = prompt("Which is larger: '1' the lower 48 states or '2' Greenland?");
+q5 = prompt("Which is larger: '1' the lower 48 contiguous United States or '2' Greenland?");
 if (parseInt(q5) === 1) {
   document.write('<p>Correct: The lower 48 states are 3.73 times larger than Greenland</p>');
   numCorrect +=1;
